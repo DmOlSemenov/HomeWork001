85503d1a247a6d29ee6bf165af1e1efdfaa85f79
Dmitriy <semenovsdo@mail.ru>
2023-02-01 23:19:38 +0300

    Задача № 6

---
+++ HomeWork001_Part3/Program.cs
@@ -0,0 +1,5 @@
+﻿//Задача № 6
+Console.WriteLine("Введите число № 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 % 2 == 0) Console.Write("Четное");
+if (number1 % 2 > 0) Console.Write("Нечетное");
