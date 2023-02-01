8d363465866cf304a399de0440c07dc89a38ba76
Dmitriy <semenovsdo@mail.ru>
2023-02-01 22:38:40 +0300

    Задача № 2

---
+++ HomeWork001/Program.cs
@@ -0,0 +1,10 @@
+﻿//Задача № 2
+Console.WriteLine("Введите число № 1 ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число № 2 ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number1 > max) max = number1;
+if (number2 > max) max = number2;
+Console.WriteLine("max =  ");
+Console.Write(max);
