9cfcaa7b203e8968a0b5a682bb014feea24e79aa
Dmitriy <semenovsdo@mail.ru>
2023-02-01 23:59:30 +0300

    Задача № 8

---
+++ HomeWork/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача № 8
+Console.WriteLine("Введите число N ");
+int N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+Console.Write("Четные числа: ");
+while (count < N)
+{
+    if (count % 2 == 0) Console.Write(count); 
+    Console.Write(", ");
+    count = count + 1;
+    if (count % 2 > 0) count = count + 1;
+}
